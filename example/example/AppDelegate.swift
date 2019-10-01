8a6bfe88d976044b7eb8b9cab6dcc545e117abf9
Cristian Novoa <novyta@Cristians-Mac.local>
2019-10-01 14:06:04 -0700

    add tint color

--- example/example/AppDelegate.swift
+++ example/example/AppDelegate.swift
@@ -15,8 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let tintColor = UIColor(red: 249/255, green: 149/255, blue: 38/255, alpha: 1.0)
+        
+        
         UITabBar.appearance().backgroundColor = .white
-        UITabBar.appearance().tintColor = .yellow
+        UITabBar.appearance().tintColor = tintColor
         // Override point for customization after application launch.
         return true
     }
