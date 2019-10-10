988ab4f7ddc9fd907402b2a30dbe983b293691a6
ccnovoa11 <cc.novoa11@uniandes.edu.co>
2019-10-10 07:48:42 -0500

    Sign in page

---
+++ example/example/view controllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  example
+//
+//  Created by Cristian Novoa on 10/10/19.
+//  Copyright © 2019 Cristian Novoa. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
